dc32149a83fe7a4941d90dbd8b23bba146beb5cb
youyoushirokunariyukuyamagiwa <hinatakakkokari@gmail.com>
2022-11-24 14:04:25 +0900

    マルチコプターのpayloadに9/1.4かけただけ

--- model/multicopter.py
+++ model/multicopter.py
@@ -9,12 +9,12 @@ class Multi(airframe.Airframe):
     
     def consum_f_high(self,payload_kg):
         payload_lb = payload_kg * 2.205 
-        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120
+        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
 
     def consum_f_low(self,payload_kg):
         payload_lb = payload_kg * 2.205 
-        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120
+        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
 
     def consum_h(self,payload_kg):
         payload_lb = payload_kg * 2.205 
-        return self.takeOffTime_s*(2.297 * payload_lb + 3.879) / 100 / 60 *321120
+        return self.takeOffTime_s*(2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
