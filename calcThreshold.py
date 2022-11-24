b86c55fa171c655e289c37f9d769d7cb2812de23
youyoushirokunariyukuyamagiwa <hinatakakkokari@gmail.com>
2022-11-24 15:08:39 +0900

    multicopterのB消費関数を1次式化

--- calcThreshold.py
+++ calcThreshold.py
@@ -7,6 +7,6 @@ m = multi.Multi()
 for payload_g in range(0,1100,100):
     payload_kg = payload_g / 1000
     time = (v.consum_h(payload_kg) - m.consum_h(payload_kg))/(m.consum_f_high(payload_kg) - v.consum_f_high(payload_kg))
-    #print("payload", payload_kg, ": vtol_h:",v.consum_h(payload_kg),": multi_h:",m.consum_h(payload_kg),
-    #": multi_f:",m.consum_f_high(payload_kg),": vtol_f:",v.consum_f_high(payload_kg),":time :", time)
+    print("payload", payload_kg, ": vtol_h:",v.consum_h(payload_kg),": multi_h:",m.consum_h(payload_kg),
+    ": multi_f:",m.consum_f_high(payload_kg),": vtol_f:",v.consum_f_high(payload_kg),":time :", time)
     
--- model/multicopter.py
+++ model/multicopter.py
@@ -8,13 +8,10 @@ class Multi(airframe.Airframe):
         self.takeOffTime_s = 60
     
     def consum_f_high(self,payload_kg):
-        payload_lb = payload_kg * 2.205 
-        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
+        return 1742.6*payload_kg + 1334.6
 
     def consum_f_low(self,payload_kg):
-        payload_lb = payload_kg * 2.205 
-        return (2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
+        return 1742.6*payload_kg + 1334.6
 
     def consum_h(self,payload_kg):
-        payload_lb = payload_kg * 2.205 
-        return self.takeOffTime_s*(2.297 * payload_lb + 3.879) / 100 / 60 * 321120 *9/1.4
+        return self.takeOffTime_s * (1742.6*payload_kg + 1334.6)
