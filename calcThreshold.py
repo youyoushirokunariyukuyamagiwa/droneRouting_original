a7df82d8aa700d2130f927234b30b0ae6c5db004
youyoushirokunariyukuyamagiwa <hinatakakkokari@gmail.com>
2022-11-24 16:50:58 +0900

    閾値計算の変数化

--- calcThreshold.py
+++ calcThreshold.py
@@ -6,7 +6,12 @@ m = multi.Multi()
 
 for payload_g in range(0,1100,100):
     payload_kg = payload_g / 1000
-    time = (v.consum_h(payload_kg) - m.consum_h(payload_kg))/(m.consum_f_high(payload_kg) - v.consum_f_high(payload_kg))
-    print("payload", payload_kg, ": vtol_h:",v.consum_h(payload_kg),": multi_h:",m.consum_h(payload_kg),
-    ": multi_f:",m.consum_f_high(payload_kg),": vtol_f:",v.consum_f_high(payload_kg),":time :", time)
+    vh = v.consum_h(payload_kg)
+    vf = v.consum_f_low(payload_kg)
+    mh = m.consum_h(payload_kg)
+    mf = m.consum_f_low(payload_kg)
+    time = (vh - mh)/(mf/m.speed_m_s - vf)
+    print("payload", payload_kg, ": vtol_h:",vh,": multi_h:",mh,
+    ": multi_f:",mf,": vtol_f:",vf,":time :", time)
+    #print("payload : ", payload_kg, "kg ,time : ", time, "s")
     
