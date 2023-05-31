e76d33047a0231e5f3295df102c5317ecf934f93
youyoushirokunariyukuyamagiwa <hinatakakkokari@gmail.com>
2023-05-31 22:11:08 +0900

    calcPulsBC()のバグを修正など

--- model/airframe.py
+++ model/airframe.py
@@ -1,8 +1,8 @@
 class Airframe:
     
     def __init__(self):
-        self.speed_m_s = 13.89  # 機体速度50km/h
-        self.maxPayload_g = 10000000
+        self.speed_km_m = 0.8333  # 機体速度50km/h
+        self.maxPayload_kg = 1
 
     def calcBattery_f(self,distance,payload_kg):
         pass
--- model/vtol.py
+++ model/vtol.py
@@ -11,21 +11,21 @@ class Vtol(airframe.Airframe):
     
     #固定翼モードでの1分あたりの消費電力割合(％/分)
     def consum_f(self,payload_kg):
-        return 0.309102509*payload_kg+84.926/40
+        return 0.309102509*payload_kg+2.12315
 
     #回転翼モードで行う離着陸1回あたり（30×２s)にかかる消費電力割合（％/１回）
     def consum_h(self,payload_kg):
         return 1.8721*payload_kg+15.07
 
     def calcBattery_f(self,distance_km,payload_kg):
-        battery = self.consum_f(payload_kg)*distance_km/self.s + self.consum_h(payload_kg)
+        battery = self.consum_f(payload_kg)*distance_km/self.speed_km_m + self.consum_h(payload_kg)
 
         return battery
-    
+
     def addPayloadBC(self,distance_km,addPayload_kg):
         battery = 0.309102509*addPayload_kg*distance_km/self.speed_km_m + 1.8721*addPayload_kg
 
         return battery
 
     def calcFlightTime(self,distance_km):
-        return distance_km/self.s + self.takeOffTime_m
+        return distance_km/self.speed_km_m + self.takeOffTime_m
